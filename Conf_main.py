a890f81d91baaae10134dfadc0406fefa779b78d
RajSeehra <rsseehra1@sheffield.ac.uk>
2020-06-12 12:54:59 +0100

    Major update for stepwise stage scanning with laser illumination. Fast and slow methods as slow takes days. Started extracting inputs.

--- Conf_main.py
+++ Conf_main.py
@@ -11,8 +11,8 @@ from scipy import signal
 # Array sizes need to be odd to work with the convolution as a centre point needs to be determinable.
 
 ### INPUTS ###
-# PSF #
-xy_size = 401
+#  Laser PSF #
+xy_size = 201
 pixel_size = 0.005
 stack_size = 40
 
@@ -21,52 +21,71 @@ stack_size = 40
 # Made a 3D PSF
 # Each pixel = 5 nanometres.
 # Greater xy size the more PSF is contained in the array. 255 seems optimal, but 101 works and is much faster.
-radPSF = sam.radial_PSF(xy_size, pixel_size, stack_size)
-radPSF = np.moveaxis(radPSF, 0, -1)     # The 1st axis was the z-values. Now in order y,x,z.
+laserPSF = sam.radial_PSF(xy_size, pixel_size, stack_size)
+laserPSF = np.moveaxis(laserPSF, 0, -1)     # The 1st axis was the z-values. Now in order y,x,z.
 
-radPSF = radPSF / radPSF.sum()          # Equating to 1. (to do: 1 count =  1 microwatt, hence conversion to photons.)
+laserPSF = laserPSF / laserPSF.sum()          # Equating to 1. (to do: 1 count =  1 microwatt, hence conversion to photons.)
 
 
 ### SAMPLE PARAMETERS ###
 # Made a point in centre of 2D array
-point = np.zeros((201, 201, radPSF.shape[2]))
-point[150, 150, radPSF.shape[2]//2+8] = 100
-point[50, 50, radPSF.shape[2]//2+4] = 100
-point[00:100, 100, radPSF.shape[2]//2] = 1
+point = np.zeros((201, 201, laserPSF.shape[2]))
+point[50, 50, laserPSF.shape[2] // 2 + 8] = 1
+point[100, 100, laserPSF.shape[2] // 2 + 4] = 1
+point[00:100, 100, laserPSF.shape[2] // 2] = 1
 
 
 ### STAGE SCANNING SO SAMPLE IS RUN ACROSS THE PSF (OR 'LENS') ###
-scan = np.zeros((radPSF.shape[1], radPSF.shape[0], radPSF.shape[2]))
-for i in range(0, radPSF.shape[2]):
-    scan[:,:,i] = np.rot90(sam.kernel_filter_2D(radPSF[:,:,i], point[:,:,i]), 2)        # When running a larger image over a smaller one it rotates the resulting info.
+scan = np.zeros((laserPSF.shape[1], laserPSF.shape[0], laserPSF.shape[2]))
+laser_illum = np.zeros((laserPSF.shape[1], laserPSF.shape[0], laserPSF.shape[2]))
 
-# Flatten and sum z stack.
-scan_sum = np.sum(scan, 2)
+# Produce an array that will receive the 2D samples we collect.
+sums = np.zeros((point.shape[1], point.shape[0], int(point.shape[1] * point.shape[0])+1))
+counter = 0
 
 
-### DEBUGGING: CHECK FOR FLATNESS ###
-# Check for flatness in single particle.
-# check = scan[:,:,100]-radPSF[:,:,100]
-# print("minimum: " + str(np.min(check)), "maximum: " + str(np.max(check)))
+# Iterates across the array to produce arrays illuminated by the sample, with a laser blurred by the first lens.
+for x in range(0, point.shape[1]+1):
+    for y in range(0, point.shape[0]+1):
+        laser_illum = sam.array_multiply(point, laserPSF, x, y)
+        # Convolute the produced array with the PSF to simulate the second lens.
+        for i in range(0, point.shape[2]):
+            scan[:,:,i] = signal.fftconvolve(laserPSF[:,:,i], laser_illum[:,:,i], mode="same")
+            # scan[:,:,i] = np.rot90(sam.kernel_filter_2D(laserPSF[:, :, i], laser_illum[:, :, i]), 2)        # When running a larger image over a smaller one it rotates the resulting info.
+        print(x,y)
+        # Flatten and sum z stack.
+        z_sum = np.sum(scan, 2)
+
+        # Multiply by pinhole
+        circle_pinhole = sam.circle_mask(scan, 20, (x, y))  # Produces a simple circle mask centred at x,y.
+        pinhole_sum = z_sum * circle_pinhole
+
+        # Add to the collection array
+        sums[:,:,counter] = pinhole_sum
+        counter = counter + 1
+
+
+# REMAKE IN PROGRESS##˙^¨¨©˙åø¨¨©ƒø©å
+
 
 ### SPACIAL FILTER PARAMETERS ###
-# 1/0 pinhole to make.!!!---> with moveability relative to the centre. circle.
-circle_pinhole = sam.circle_mask(scan, 20, (200, 200))  # Shifting the centre, offsets the image by the same value.
+# 1/0 pinhole to make.!!!---> with mobility relative to the centre. circle.
+# circle_pinhole = sam.circle_mask(scan, 20, (200, 200))  # Shifting the centre, offsets the image by the same value.
 
 # # Made a gaussian. (Our spacial filter)  NEED SCALE FOR THIS PINHOLE AS IT WILL VASTLY IMPACT QUALITY.
-spacial_filter = sam.Gaussian_Map((scan.shape[1], scan.shape[0]), 0, 0, 0, 1, 1)
-spacial_filter[spacial_filter < 0.05] = 0
-spacial_filter = spacial_filter / np.sum(spacial_filter)
+# spacial_filter = sam.Gaussian_Map((scan.shape[1], scan.shape[0]), 0, 0, 0, 1, 1)
+# spacial_filter[spacial_filter < 0.05] = 0
+# spacial_filter = spacial_filter / np.sum(spacial_filter)
 
 ### PINHOLE ###
-pinhole = np.zeros((scan.shape[1], scan.shape[0], scan.shape[2]))
+# pinhole = np.zeros((scan.shape[1], scan.shape[0], scan.shape[2]))
 # Each array in the 3D scan is convoluted with the spacial filter individually as they are each snapshots in space.
-pinhole = np.rot90(sam.kernel_filter_2D(circle_pinhole, scan_sum), 2)
+# pinhole = np.rot90(sam.kernel_filter_2D(circle_pinhole, scan_sum), 2)
 # pinhole = sam.kernel_filter_2D(spacial_filter, scan_sum)
 # for i in range(0, scan.shape[2]):
 #     pinhole[:, :, i] = np.rot90(sam.kernel_filter_2D(circle_pinhole, scan[:, :, i]), 2)
 
-plt.imshow(pinhole)
+# plt.imshow(pinhole)
 # flat = np.sum(pinhole, axis=2)
 ### PLOTTING ###
 # plt.imshow(flat)
--- Tester.py
+++ Tester.py
@@ -1,15 +1,77 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import Confocal_Samurai as sam
+from scipy import signal
+from PIL import Image
 
-centre_x = 400
-centre_y = 500
-a_x = -centre_x  # distance of left edge of screen relative to centre x.
-b_x = 1000 - centre_x  # distance of right edge of screen relative to centre x.
-a_y = -centre_y  # distance of top edge of screen relative to centre y.
-b_y = 1000 - centre_y  # distance of bottom edge of screen relative to centre y.
+# centre_x = 400
+# centre_y = 500
+# a_x = -centre_x  # distance of left edge of screen relative to centre x.
+# b_x = 1000 - centre_x  # distance of right edge of screen relative to centre x.
+# a_y = -centre_y  # distance of top edge of screen relative to centre y.
+# b_y = 1000 - centre_y  # distance of bottom edge of screen relative to centre y.
+#
+# r = 100
+# # Produce circle mask, ones grid = to original file and cut out.
+# y, x = np.ogrid[a_x:b_x, a_y:b_y]  # produces a list which collapses to 0 at the centre in x and y
+# mask = x * x + y * y <= r * r  #
+# ones = np.ones((1000, 1000))
+# ones[mask] = 0
 
-r = 100
-# Produce circle mask, ones grid = to original file and cut out.
-y, x = np.ogrid[a_x:b_x, a_y:b_y]  # produces a list which collapses to 0 at the centre in x and y
-mask = x * x + y * y <= r * r  #
-ones = np.ones((1000, 1000))
-ones[mask] = 0
+x = np.load("conf2.npy")
+# print(np.max(x))
+# sam.savetiffs("output.tif", x)
+# array= np.zeros((201,201))
+
+# for i in range(0, x.shape[2]):
+#     array[i%201, i//201] = np.sum(x[:,:,i])
+#     print(i//201, i%201)
+#
+# plt.imshow(array)
+# plt.imshow(x[:,:,10000])
+# plt.show()
+
+
+#
+# ### INPUTS ###
+# #  Laser PSF #
+# xy_size = 201
+# pixel_size = 0.005
+# stack_size = 40
+#
+#
+# ### PSF Generation ###
+# # Made a 3D PSF
+# # Each pixel = 5 nanometres.
+# # Greater xy size the more PSF is contained in the array. 255 seems optimal, but 101 works and is much faster.
+# laserPSF = sam.radial_PSF(xy_size, pixel_size, stack_size)
+# laserPSF = np.moveaxis(laserPSF, 0, -1)     # The 1st axis was the z-values. Now in order y,x,z.
+#
+# laserPSF = laserPSF / laserPSF.sum()          # Equating to 1. (to do: 1 count =  1 microwatt, hence conversion to photons.)
+#
+#
+# ### SAMPLE PARAMETERS ###
+# # Made a point in centre of 2D array
+# point = np.zeros((201, 201, laserPSF.shape[2]))
+# point[50, 50, laserPSF.shape[2] // 2 + 8] = 1
+# point[100, 100, laserPSF.shape[2] // 2 + 4] = 1
+# point[00:100, 100, laserPSF.shape[2] // 2] = 1
+#
+# a = sam.array_multiply(point, laserPSF, 125, 125)
+#
+# scan = scan = np.zeros((laserPSF.shape[1], laserPSF.shape[0], laserPSF.shape[2]))
+#
+#
+# for i in range(0, point.shape[2]):
+#     scan[:,:,i] = signal.fftconvolve(laserPSF[:, :, i], a[:, :, i], mode="same")
+#
+# # Multiply by pinhole
+# summer = np.sum(scan,2)
+# circle_pinhole = sam.circle_mask(scan, 20, (125, 125))  # Produces a simple circle mask centred at x,y.
+# pinhole_sum = summer * circle_pinhole
+#
+# plt.subplot(121)
+# plt.imshow(summer)
+# plt.subplot(122)
+# plt.imshow(pinhole_sum)
+# plt.show()
